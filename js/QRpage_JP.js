471bd952ee7b921a7c2cedc3ee42fb1fb6621947
nh <knkn0928@naver.com>
2024-05-23 17:59:06 +0900

    으아아아아아

--- js/QRpage_JP.js
+++ js/QRpage_JP.js
@@ -1,56 +1,57 @@
-$(window).on('load', function () {
-    fullPage();
+$('.quick_menu a').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 0;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+        scrollTop: target
+    }, 500);
+    event.preventDefault();
 });
 
-function fullPage() {
-    let currentSection = 0;
-    const $sections = $('.section'); // 모든 섹션 요소
-    const $menu =  $('.quick_menu > li'); // 메뉴 항목
-    const $footer = $('footer'); // 페이지 하단 영역
-    let menuHeight = $('header').height(); // 상단 메뉴 높이
-    const numSections = $sections.length; // 섹션의 개수
 
-    //메뉴 클릭 이벤트 핸들러
-    function clickHandler(e, targetSectionIndex) {
-        e.preventDefault();
-        const offset = $sections.eq(targetSectionIndex).offset().top;
-        currentSection = targetSectionIndex;
-        // 해당 섹션으로 스크롤 이동
-        $('html,body').animate({ scrollTop: offset - menuHeight }, 500);
-    }
+$(function(){
+    var fixScroll = 0;
+    $(window).scroll(function(event){
+        var COFFILM = $("#COFFILM").outerHeight();
+        var IMPAPER =  $("#IMPAPER").outerHeight();
+        var IMBIO =  $("#IMBIO").outerHeight();
+        var IMMONO =  $("#IMMONO").outerHeight();
 
-    // 섹션 스크롤 함수
-    function scrollToSection(sectionIndex) {
-        const targetPosition = sectionIndex === numSections ? $footer.offset().top : $sections.eq(sectionIndex).offset().top;
-        //특정 섹션으로 스크롤 이동
-        $('html, body').animate({
-            scrollTop: targetPosition - menuHeight
-        }, 500, function () {
-            isScrolling = false;
-        });
-    }
+        var scroll = $(this).scrollTop();
+        if (scroll < COFFILM){
+            $(".COFFILM").addClass("active");
+        }
+        else {
+            $(".COFFILM").removeClass("active");
+        }
+        fixScroll = scroll;
 
-    // 메뉴 클릭 이벤트 핸들러
-    $menu.children('a').on('click', function (e) {
-        const targetSectionIndex = $sections.index($($(this).attr('href')));
-        clickHandler(e, targetSectionIndex);
+        var scroll = $(this).scrollTop();
+        if (scroll > IMPAPER){
+            $(".IMPAPER").addClass("active");
+        }
+        else {
+            $(".IMPAPER").removeClass("active");
+        }
+        fixScroll = scroll;
+
+        var scroll = $(this).scrollTop();
+        if (scroll > IMBIO){
+            $(".IMBIO").addClass("active");
+        }
+        else {
+            $(".IMBIO").removeClass("active");
+        }
+        fixScroll = scroll;
+
+        var scroll = $(this).scrollTop();
+        if (scroll > IMMONO){
+            $(".IMMONO").addClass("active");
+        }
+        else {
+            $(".IMMONO").removeClass("active");
+        }
+
+        fixScroll = scroll;
     });
-
-    // // 마우스 휠 이벤트 핸들러
-    // $(window).on('wheel', handleScroll);
-
-        // 스크롤 이벤트 핸들러를 통해 활성 메뉴 업데이트
-        $(window).scroll(function(){
-            let scltop = $(window).scrollTop() + menuHeight;
-            $.each($sections, function(idx, item){
-                let targetTop = $(this).offset().top;
-                if (targetTop <= scltop) {
-                    $menu.removeClass('active');
-                    $menu.eq(idx).addClass('active');
-                }
-            })
-            if (Math.round( $(window).scrollTop()) == $(document).height() - $(window).height()) {
-                $menu.last().addClass('active').siblings().removeClass('active');
-            }
-        }).scroll();
-    }
+});
